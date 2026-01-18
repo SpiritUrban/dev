4aab41178cb91086f5e4b9ebc114ea50cfab93f9
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2026-01-18 11:20:29 +0200

    repair

--- src/features/timeline/timelineSelectors.ts
+++
@@ -1,5 +0,0 @@
-import type { RootState } from '../../app/store';
-
-export const selectVisibleItems = (state: RootState) => state.timeline.visibleItems;
-export const selectUniqueTechnologies = (state: RootState) => state.timeline.uniqueTechnologies;
-export const selectLastUpdated = (state: RootState) => state.timeline.lastUpdated;
